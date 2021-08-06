ed32adabed3df4063f498d0ffe2bc921cab671db
Alexander Castro <alexander.castro01@uptc.edu.co>
2021-08-06 15:30:01 -0500

    Se crea la estructura inicial del proyecto

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <CharacterCard/>
+</template>
+
+<script>
+import CharacterCard from './components/CharacterCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CharacterCard
+  }
+}
+</script>
+
+<style>
+
+</style>
