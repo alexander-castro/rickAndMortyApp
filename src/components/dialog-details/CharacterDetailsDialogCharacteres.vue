84034ba4f31872080296698ecf83e04ab95cb2c7
Alexander Castro <alexander.castro01@uptc.edu.co>
2021-08-09 23:42:48 -0500

    Fix en tarjetas de personajes

--- src/components/dialog-details/CharacterDetailsDialogCharacteres.vue
+++ src/components/dialog-details/CharacterDetailsDialogCharacteres.vue
@@ -4,7 +4,7 @@
   </q-card-section>
   <q-card-section class="row justify-between q-gutter-xs">
     <div class="col-md-3" v-for="character in characterList" :key="character.id">
-      <character-card v-bind="character" @show-details-modal="showDetailsModal" />
+      <character-card v-bind="character" />
     </div>
   </q-card-section>
 </template>
